576cc2bed76f457fe897e646f129a6ea17d08873
Pascal Haupt <75530910+haupt-pascal@users.noreply.github.com>
2025-01-18 11:19:44 +0100

    modified landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
     <Navigation />
-    <div class="container mx-auto">
+    <div class="container mx-auto mt-24">
         <div class="flex flex-col items-center justify-center">
             <h1 class="text-4xl font-bold text-center">ts-spellchecker</h1>
-            <p class="text-center text-3xl font-bold underline">
+            <p class="text-center mt-6 w-2/3">
                 ts-spellchecker is an open-source spellchecker written in Nuxt
                 with Typescript and Bun. It is a simple spellchecker that can be
                 used to check the spelling of a word or a sentence.
